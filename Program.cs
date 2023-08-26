209ed245c2340132c2e50089e5090b9f887d156a
sananeo0 <sananeo@code.edu.az>
2023-08-27 02:26:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ThirdTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "java";
+            input = input.ToUpper();
+           char[] charArry = input.ToCharArray();
+            Array.Reverse(charArry);           
+            Console.WriteLine(charArry);
+        }
+    }
+}
